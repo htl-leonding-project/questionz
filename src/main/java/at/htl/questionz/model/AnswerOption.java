33f6d7814af7b61366978de49a48c73e4d015202
FitzingerJ <jacki.fitzinger@gmail.com>
2020-12-03 10:45:18 +0100

    #7 AnswerOption JPA ready TODO!!

--- src/main/java/at/htl/questionz/model/AnswerOption.java
+++ src/main/java/at/htl/questionz/model/AnswerOption.java
@@ -1,71 +1,82 @@
 package at.htl.questionz.model;
 
+import javax.persistence.*;
+
+@Entity
 public class AnswerOption {
 
-    private Long ao_id;
-    private String ao_text;
-    private int ao_value;
-    private int ao_sequenceNumber;
-    private Question ao_question;
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ao_id")
+    private Long id;
+    @Column(name = "ao_text")
+    private String text;
+    @Column(name = "ao_value")
+    private int value;
+    @Column(name = "ao_sequenceNumber")
+    private int sequenceNumber;
+
+    //TODO: FOREIGN KEYS IN JPA
+    @Column(name = "ao_question")
+    private Question question;
 
     public AnswerOption() {
     }
 
-    public AnswerOption(Long ao_id, String ao_text, int ao_value, int ao_sequenceNumber, Question ao_question) {
-        this.ao_id = ao_id;
-        this.ao_text = ao_text;
-        this.ao_value = ao_value;
-        this.ao_sequenceNumber = ao_sequenceNumber;
-        this.ao_question = ao_question;
+    public AnswerOption(Long id, String text, int value, int sequenceNumber, Question question) {
+        this.id = id;
+        this.text = text;
+        this.value = value;
+        this.sequenceNumber = sequenceNumber;
+        this.question = question;
     }
 
-    public Long getAo_id() {
-        return ao_id;
+    public Long getId() {
+        return id;
     }
 
-    public void setAo_id(Long ao_id) {
-        this.ao_id = ao_id;
+    public void setId(Long ao_id) {
+        this.id = ao_id;
     }
 
-    public String getAo_text() {
-        return ao_text;
+    public String getText() {
+        return text;
     }
 
-    public void setAo_text(String ao_text) {
-        this.ao_text = ao_text;
+    public void setText(String ao_text) {
+        this.text = ao_text;
     }
 
-    public int getAo_value() {
-        return ao_value;
+    public int getValue() {
+        return value;
     }
 
-    public void setAo_value(int ao_value) {
-        this.ao_value = ao_value;
+    public void setValue(int ao_value) {
+        this.value = ao_value;
     }
 
-    public int getAo_sequenceNumber() {
-        return ao_sequenceNumber;
+    public int getSequenceNumber() {
+        return sequenceNumber;
     }
 
-    public void setAo_sequenceNumber(int ao_sequenceNumber) {
-        this.ao_sequenceNumber = ao_sequenceNumber;
+    public void setSequenceNumber(int ao_sequenceNumber) {
+        this.sequenceNumber = ao_sequenceNumber;
     }
 
-    public Question getAo_question() {
-        return ao_question;
+    public Question getQuestion() {
+        return question;
     }
 
-    public void setAo_question(Question ao_question) {
-        this.ao_question = ao_question;
+    public void setQuestion(Question ao_question) {
+        this.question = ao_question;
     }
 
     @Override
     public String toString() {
         return "AnswerOption{" +
-                "ao_id=" + ao_id +
-                ", ao_String='" + ao_text + '\'' +
-                ", ao_value=" + ao_value +
-                ", ao_question=" + ao_question +
+                "ao_id=" + id +
+                ", ao_String='" + text + '\'' +
+                ", ao_value=" + value +
+                ", ao_question=" + question +
                 '}';
     }
 }
