20aaca0ec823e03121c77e018718d0dffa4452db
FitzingerJ <jacki.fitzinger@gmail.com>
2020-12-03 10:53:57 +0100

    ChosenOption renamed Entities

--- src/main/java/at/htl/questionz/model/ChosenOption.java
+++ src/main/java/at/htl/questionz/model/ChosenOption.java
@@ -8,49 +8,40 @@ public class ChosenOption {
 
     //TODO: JPA FOREIGN KEYS
     @Column(name = "co_answerOption")
-    private AnswerOption co_answerOption;
+    private AnswerOption answerOption;
     @Column(name = "co_answer")
-    private Answer co_answer;
+    private Answer answer;
 
     public ChosenOption() {
     }
 
-    public ChosenOption(AnswerOption co_answerOption, Answer co_answer, Question co_question) {
-        this.co_answerOption = co_answerOption;
-        this.co_answer = co_answer;
-        this.co_question = co_question;
+    public ChosenOption(AnswerOption answerOption, Answer answer) {
+        this.answerOption = answerOption;
+        this.answer = answer;
     }
 
-    public AnswerOption getCo_answerOption() {
-        return co_answerOption;
+    public AnswerOption getAnswerOption() {
+        return answerOption;
     }
 
-    public void setCo_answerOption(AnswerOption co_answerOption) {
-        this.co_answerOption = co_answerOption;
+    public void setAnswerOption(AnswerOption co_answerOption) {
+        this.answerOption = co_answerOption;
     }
 
-    public Answer getCo_answer() {
-        return co_answer;
+    public Answer getAnswer() {
+        return answer;
     }
 
-    public void setCo_answer(Answer co_answer) {
-        this.co_answer = co_answer;
+    public void setAnswer(Answer co_answer) {
+        this.answer = co_answer;
     }
 
-    public Question getCo_question() {
-        return co_question;
-    }
-
-    public void setCo_question(Question co_question) {
-        this.co_question = co_question;
-    }
 
     @Override
     public String toString() {
         return "ChosenOption{" +
-                "co_answerOption=" + co_answerOption +
-                ", co_answer=" + co_answer +
-                ", co_question=" + co_question +
+                "co_answerOption=" + answerOption +
+                ", co_answer=" + answer +
                 '}';
     }
 }
