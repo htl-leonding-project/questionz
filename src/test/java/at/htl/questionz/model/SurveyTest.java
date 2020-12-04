8662fb4b8b365b202a6855d850e9987d23c09592
FitzingerJ <jacki.fitzinger@gmail.com>
2020-12-04 12:03:51 +0100

    #8 create Survey Test

--- src/test/java/at/htl/questionz/model/SurveyTest.java
+++ src/test/java/at/htl/questionz/model/SurveyTest.java
@@ -1,7 +1,26 @@
 package at.htl.questionz.model;
 
-import static org.junit.jupiter.api.Assertions.*;
+import at.htl.model.Questionnaire;
+import at.htl.model.Survey;
+import at.htl.model.Teacher;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class SurveyTest {
 
+    Teacher teacher = new Teacher("Max Mustermann");
+    Questionnaire questionnaire = new Questionnaire("Schüler zufriedenheit", "In diesem Fragebogen wird die zufriedenheit der Schüler abgefragt.");
+    Survey survey = new Survey(LocalDate.of(2020, 8,1), teacher, questionnaire);
+
+    @Test
+    void createSurvey_Test() {
+        //assert
+        assertThat(survey.getDate()).isEqualTo(LocalDate.of(2020, 8,1));
+        assertThat(survey.getQuestionnaire().getName()).isEqualTo("Schüler zufriedenheit");
+        assertThat(survey.getQuestionnaire().getDesc()).isEqualTo("In diesem Fragebogen wird die zufriedenheit der Schüler abgefragt.");
+        assertThat(survey.getTeacher().getName()).isEqualTo("Max Mustermann");
+    }
 }
