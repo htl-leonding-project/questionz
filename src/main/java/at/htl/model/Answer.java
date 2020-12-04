123e9d1763bce8ee6efd7d9a9f0ad1466f71b552
FitzingerJ <jacki.fitzinger@gmail.com>
2020-12-04 11:01:29 +0100

    #7 fixed Answer Variable names

--- src/main/java/at/htl/model/Answer.java
+++ src/main/java/at/htl/model/Answer.java
@@ -7,65 +7,65 @@ public class Answer {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column("a_id")
-    private Long a_id;
+    private Long id;
     @Column("a_text")
-    private String a_text;
+    private String text;
 
     //TODO JPA FOREIGN KEYS
     @Column("a_transaction")
-    private Transaction a_transaction;
+    private Transaction transaction;
     @Column("a_question")
-    private at.htl.questionz.model.Question a_question;
+    private Question question;
 
     public Answer() {
     }
 
-    public Answer(Long a_id, String a_text, Transaction a_transaction, at.htl.questionz.model.Question a_question) {
-        this.a_id = a_id;
-        this.a_text = a_text;
-        this.a_transaction = a_transaction;
-        this.a_question = a_question;
+    public Answer(Long id, String text, Transaction transaction, Question question) {
+        this.id = id;
+        this.text = text;
+        this.transaction = transaction;
+        this.question = question;
     }
 
-    public Long getA_id() {
-        return a_id;
+    public Long getId() {
+        return id;
     }
 
-    public void setA_id(Long a_id) {
-        this.a_id = a_id;
+    public void setId(Long a_id) {
+        this.id = a_id;
     }
 
-    public String getA_text() {
-        return a_text;
+    public String getText() {
+        return text;
     }
 
-    public void setA_text(String a_text) {
-        this.a_text = a_text;
+    public void setText(String a_text) {
+        this.text = a_text;
     }
 
-    public Transaction getA_transaction() {
-        return a_transaction;
+    public Transaction getTransaction() {
+        return transaction;
     }
 
-    public void setA_transaction(Transaction a_transaction) {
-        this.a_transaction = a_transaction;
+    public void setTransaction(Transaction a_transaction) {
+        this.transaction = a_transaction;
     }
 
-    public at.htl.questionz.model.Question getA_question() {
-        return a_question;
+    public at.htl.questionz.model.Question getQuestion() {
+        return question;
     }
 
-    public void setA_question(at.htl.questionz.model.Question a_question) {
-        this.a_question = a_question;
+    public void setQuestion(at.htl.questionz.model.Question a_question) {
+        this.question = a_question;
     }
 
     @Override
     public String toString() {
         return "Answer{" +
-                "a_id=" + a_id +
-                ", a_text='" + a_text + '\'' +
-                ", a_transaction=" + a_transaction +
-                ", a_question=" + a_question +
+                "a_id=" + id +
+                ", a_text='" + text + '\'' +
+                ", a_transaction=" + transaction +
+                ", a_question=" + question +
                 '}';
     }
 }
