5bf1f78c784c246f323700d4b5359d9a34a0ff1b
FitzingerJ <jacki.fitzinger@gmail.com>
2020-12-04 10:37:45 +0100

    #8 create Teacher Test

--- src/test/java/at/htl/questionz/model/TeacherTest.java
+++ src/test/java/at/htl/questionz/model/TeacherTest.java
@@ -1,7 +1,20 @@
 package at.htl.questionz.model;
 
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
 class TeacherTest {
 
+    @Test
+    @Order(10)
+    void createTeacher_Test() {
+        //arrange
+        Teacher teacher = new Teacher("Max Mustermann");
+
+        //assert
+        assertThat(teacher.getName()).isEqualTo("Max Mustermann");
+    }
 }
