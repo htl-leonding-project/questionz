5334242d2eed8d617883a4e3c9a17859fe4e34b6
Tobert <tobiashoefl@gmail.com>
2020-12-04 11:47:59 +0100

    #5 TeacherRepository.java - add method done

--- src/main/java/at/htl/control/TeacherRepository.java
+++ src/main/java/at/htl/control/TeacherRepository.java
@@ -1,4 +1,20 @@
 package at.htl.control;
 
+import at.htl.model.Teacher;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+
+@ApplicationScoped
 public class TeacherRepository {
+
+    @Inject
+    EntityManager em;
+
+    public void add(Teacher newTeacher) {
+        em.persist(newTeacher);
+    }
+    
 }
+
