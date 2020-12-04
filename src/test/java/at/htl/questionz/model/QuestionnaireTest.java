39dfd93691f4ced1cb19e19969ec83f94802d009
FitzingerJ <jacki.fitzinger@gmail.com>
2020-12-04 12:08:12 +0100

    #8 create Questionnaire Test

--- src/test/java/at/htl/questionz/model/QuestionnaireTest.java
+++ src/test/java/at/htl/questionz/model/QuestionnaireTest.java
@@ -1,7 +1,18 @@
 package at.htl.questionz.model;
 
 import at.htl.model.Questionnaire;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class QuestionnaireTest {
-    Questionnaire questionnaire = new Questionnaire();
+
+    Questionnaire questionnaire = new Questionnaire("Schüler zufriedenheit", "In diesem Fragebogen wird die zufriedenheit der Schüler abgefragt.");
+
+    @Test
+    void createQuestionnaire_Test() {
+        //assert
+        assertThat(questionnaire.getName()).isEqualTo("Schüler zufriedenheit");
+        assertThat(questionnaire.getDesc()).isEqualTo("In diesem Fragebogen wird die zufriedenheit der Schüler abgefragt.");
+    }
 }
