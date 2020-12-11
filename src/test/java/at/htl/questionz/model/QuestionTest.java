2fcb2e13100314a71715bf08aeebc802bf737744
FitzingerJ <jacki.fitzinger@gmail.com>
2020-12-11 11:45:18 +0100

    #8 createQuestion_Test

--- src/test/java/at/htl/questionz/model/QuestionTest.java
+++ src/test/java/at/htl/questionz/model/QuestionTest.java
@@ -3,10 +3,13 @@ package at.htl.questionz.model;
 import at.htl.model.Question;
 import at.htl.model.QuestionType;
 import at.htl.model.Questionnaire;
+import org.junit.jupiter.api.Test;
 
 import javax.sql.rowset.serial.SerialBlob;
 import java.sql.Blob;
 import java.sql.SQLException;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class QuestionTest {
 
@@ -15,6 +18,14 @@ class QuestionTest {
     Questionnaire questionnaire = new Questionnaire("Schüler zufriedenheit", "In diesem Fragebogen wird die zufriedenheit der Schüler abgefragt.");
     Question question = new Question("An diesem Lehrer gefällt mir...", 1, blob, QuestionType.FREETEXT, questionnaire);
 
-    QuestionTest() throws SQLException {
+    QuestionTest() throws SQLException {}
+
+    @Test
+    void createQuestion_Test() throws SQLException {
+        assertThat(question.getText()).isEqualTo("An diesem Lehrer gefällt mir...");
+        assertThat(question.getSequenceNumber()).isEqualTo(1);
+        assertThat(question.getImage()).isEqualTo(new SerialBlob(byteArray));
+        assertThat(question.getType()).isEqualTo(QuestionType.FREETEXT);
+        assertThat(question.getQuestionnaire()).isEqualTo(questionnaire);
     }
 }
