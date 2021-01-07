18d6d80fd05204e18bf8086d70e6ebd89fc5b68a
BuildTools <unconfigured@null.spigotmc.org>
2021-01-07 10:41:54 +0100

    #7 Added Table annotation to Answer.java

--- src/main/java/at/htl/model/Answer.java
+++ src/main/java/at/htl/model/Answer.java
@@ -6,6 +6,7 @@ import javax.persistence.*;
 @Table(name = "answer")
 public class Answer {
 
+
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "a_id")
     private Long id;
